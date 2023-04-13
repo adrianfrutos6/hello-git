dbac4cb7f8b2ee0afda4d0fb23f69c5863143041
adrian.frutos <adrian.frutos@mimacom.com>
2023-04-13 16:34:52 +0200

    Se actualiza el texto del print del helloGit.java

--- src/main/java/org/example/helloGit.java
+++ src/main/java/org/example/helloGit.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class helloGit {
     public static void main(String[] args) {
-        System.out.println("Hello Git!");
+        System.out.println("New Hello Git!");
     }
 }
