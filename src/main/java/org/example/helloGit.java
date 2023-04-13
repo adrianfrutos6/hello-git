39fcfcd0712b50b0d1aa0847c333c68ebfb08e91
adrian.frutos <adrian.frutos@mimacom.com>
2023-04-13 16:25:38 +0200

    Este es mi segundo commit

--- src/main/java/org/example/helloGit.java
+++ src/main/java/org/example/helloGit.java
@@ -1,6 +1,6 @@
 package org.example;
 
-public class Main {
+public class helloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
     }
---
+++ src/main/java/org/example/helloGit2.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class helloGit2 {
+    public static void main(String[] args) {
+        System.out.println("Hello Git 2!");
+    }
+}
