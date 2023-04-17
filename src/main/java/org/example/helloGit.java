411a15b85296de29c7948bfa318ea1a54143581d
adrian.frutos <adrian.frutos@mimacom.com>
2023-04-17 13:41:33 +0200

    Hello Github

--- src/main/java/org/example/helloGit.java
+++ src/main/java/org/example/helloGit.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class helloGit {
     public static void main(String[] args) {
-        System.out.println("New Hello Git!");
+        System.out.println("Hello GitHub!");
     }
 }
